0edbe8fda0075948c258f7a983e8a45ac3e5af32
Magda Skowroń <mskowron0819@gmail.com>
2017-05-11 13:56:01 +0200

    test

--- js/app.jsx
+++ js/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+import { Router, Route, IndexRoute, hashHistory} from 'react-router';
 import Navigation from './nav.jsx';
 import MainSection from './main.jsx';
 import WildLife from './wildLife.jsx';
@@ -13,7 +13,7 @@ require("./style.scss");
 document.addEventListener('DOMContentLoaded',()=>{
     class App extends React.Component{
         render(){
-            return <Router history={browserHistory}>
+            return <Router history={hashHistory}>
                     <Route path='/' component={Navigation}>
                 <IndexRoute component={MainSection} />
                         <Route path='/wildLife' component={WildLife} />
